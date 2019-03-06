20c525169b54a3dd13d92d000f9d7a17cbbacdea
skugge <tor.skuggevik@gmail.com>
2019-03-06 20:18:48 +0100

    Added BroadCastReceiver, should discover available devices. Data transfer not yet implemented

---
+++ app/src/main/java/com/example/botb/WifiDirectBroadcastReceiver.java
@@ -0,0 +1,45 @@
+package com.example.botb;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.widget.Toast;
+
+public class WifiDirectBroadcastReceiver extends BroadcastReceiver {
+    private WifiP2pManager mManager;
+    private WifiP2pManager.Channel mChannel;
+    private WIFIP2P mActivitiy;
+
+    public WifiDirectBroadcastReceiver(WifiP2pManager mManager, WifiP2pManager.Channel mChannel, WIFIP2P mActivitiy){
+        this.mManager = mManager;
+        this.mChannel = mChannel;
+        this.mActivitiy = mActivitiy;
+    }
+
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+
+        if(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)){
+            int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
+            if(state == WifiP2pManager.WIFI_P2P_STATE_ENABLED){
+                Toast.makeText(context,"Wifi is ON", Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(context,"Wifi is OFF", Toast.LENGTH_SHORT).show();
+            }
+        }else if(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)){
+            //hei
+            if(mManager != null){
+                mManager.requestPeers(mChannel,mActivitiy.peerListListener);
+            }
+        }else if(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)){
+            //hei
+        }else if(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)){
+            //hei
+        }
+    }
+}
