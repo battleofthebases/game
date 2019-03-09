ce9074d78f652ea9e8f3793cdd80be60cd7c509d
tjabejohannes <tjabejv@stud.ntnu.no>
2019-03-09 15:38:01 +0100

    Adding the game activity #10

--- app/src/main/java/com/example/botb/MainActivity.java
+++ app/src/main/java/com/example/botb/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.botb;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button start = (Button) findViewById(R.id.start);
+        start.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,GameActivity.class));
+            }
+        });
     }
 }
