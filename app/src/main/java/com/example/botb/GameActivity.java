6ce1067ae19ab2cbd312ebfc29b37e725f026e16
EdvardGB <edvard_bakken@live.no>
2019-03-10 14:20:43 +0100

    Simple fragment navigation implemented

--- app/src/main/java/com/example/botb/GameActivity.java
+++ app/src/main/java/com/example/botb/GameActivity.java
@@ -1,20 +1,52 @@
 package com.example.botb;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class GameActivity extends AppCompatActivity {
+
+    private Button mainButton;
+
+    private statePageAdapter adapter;
+    private ViewPager viewPager;
+
+    private int toggle = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        mainButton = (Button) findViewById(R.id.Main_button);
+
+        adapter = new statePageAdapter(getSupportFragmentManager());
+        viewPager =  (ViewPager) findViewById(R.id.contianer);
+        setupViewPager(viewPager);
 
 
 
+        mainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewPager.setCurrentItem(toggle);
+                if (toggle == 0){toggle = 1;} else {toggle = 0;}
+            }
+        });
+    }
 
+    private void setupViewPager(ViewPager viewPager){
+        statePageAdapter adapter = new statePageAdapter(getSupportFragmentManager());
+        adapter.addFragment(new GameView(), "GameView");
+        adapter.addFragment(new OpponentView(), "OpponentView");
+        viewPager.setAdapter(adapter);
     }
 
 }
