138f3171b03dffeff14772396a1667753a6b3ffe
Tjabe Johannes van Lessen <tjabejv@stud.ntnu.no>
2019-03-10 17:08:11 +0100

    Adding the InputManager

---
+++ app/src/main/java/com/example/botb/InputManager.java
@@ -0,0 +1,45 @@
+package com.example.botb;
+
+import android.util.Log;
+
+import com.example.botb.controller.GameController;
+import com.example.botb.model.Action;
+import com.example.botb.model.Location;
+
+public class InputManager {
+
+    private final String TAG = "InputManger";
+    private static final InputManager instance = new InputManager();
+    private GameController gameController;
+    private ConnectionHandler connectionHandler;
+
+    //private constructor to avoid client applications to use constructor
+    private InputManager() {
+        gameController = new GameController();
+        //creating the connection
+        connectionHandler = new ConnectionHandler();
+    }
+
+    public static InputManager getInstance() {
+        return instance;
+    }
+
+    public void handleExternalAction(String[] actionData) {
+        Log.e(TAG, "Got string message!");
+        gameController.applyAction(false, stringToAction(actionData));
+    }
+
+    public void handleLocalAction(Action action) {
+        gameController.applyAction(true, action);
+        connectionHandler.sendMessage(actionToString(action));
+    }
+
+    private Action stringToAction(String[] actionData) {
+        // Format: "LocationX:LocationY:WeaponID"
+        return new Action(new Location(Integer.parseInt(actionData[0]), Integer.parseInt(actionData[1])), WeaponFactory.getWeapon(actionData[2]));
+    }
+    private String actionToString(Action action){
+        // Format: "LocationX:LocationY:WeaponID"
+        return ""+action.getLocation().getX()+":"+action.getLocation().getY()+":"+action.getWeapon().getWeaponType();
+    }
+}
---
+++ app/src/main/java/com/example/botb/controller/GameController.java
@@ -0,0 +1,32 @@
+package com.example.botb.controller;
+
+import com.example.botb.model.Action;
+import com.example.botb.model.Board;
+import com.example.botb.model.Game;
+import com.example.botb.model.Location;
+import com.example.botb.model.placable.Placeable;
+
+import java.util.Map;
+
+public class GameController {
+    private Game game;
+    private Board localBoard, remoteBoard;
+    public boolean startGame(boolean isLocalTurn){
+        if (localBoard != null && remoteBoard != null) {
+            game = new Game(localBoard, remoteBoard, isLocalTurn);
+            return true;
+        }
+        return false;
+    }
+    public void setLocalBoard(int width, int height, Map<Location, Placeable> placeables) {
+        localBoard = new Board(width, height, placeables);
+    }
+    public void setRemoteBoard(int width, int height, Map<Location, Placeable> placeables) {
+        remoteBoard = new Board(width, height, placeables);
+    }
+    public void applyAction(boolean localPlayer, Action action) {
+        if (game != null) {
+            game.applyAction(localPlayer, action);
+        }
+    }
+}
