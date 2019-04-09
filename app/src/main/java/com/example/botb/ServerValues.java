76d0a056432ca51a51fb99a68aa054ea3496b9d1
tjabejohannes <tjabejv@stud.ntnu.no>
2019-04-09 14:56:38 +0200

    Setting the server host to the aws service

--- app/src/main/java/com/example/botb/ServerValues.java
+++ app/src/main/java/com/example/botb/ServerValues.java
@@ -2,7 +2,7 @@ package com.example.botb;
 
 final class ServerValues {
 
-    static final String SERVER_ADDRESS = "10.22.2.217";
+    static final String SERVER_ADDRESS = "ec2-3-16-23-211.us-east-2.compute.amazonaws.com";
 
     static final String SERVER_PORT = ":8080";
 }
