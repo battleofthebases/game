d8ab521b08ecd81207acb1897705573dbf458ef4
Håvard Aasmo <havi95@gmail.com>
2019-04-30 12:55:46 +0200

    Add shots to Board model

--- app/src/main/java/com/example/botb/model/Board.java
+++ app/src/main/java/com/example/botb/model/Board.java
@@ -3,12 +3,16 @@ package com.example.botb.model;
 import com.example.botb.model.placeable.Placeable;
 import com.example.botb.model.weapon.Weapon;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Board implements Serializable {
 
     private Map<Location, Placeable> placeables;
+
+    private List<Location> shots;
 
     private int width, height;
 
@@ -16,12 +20,14 @@ public class Board implements Serializable {
         this.width = width;
         this.height = height;
         placeables = new HashMap<>();
+        shots = new ArrayList<>();
     }
 
     public Board(int width, int height, Map<Location, Placeable> placeables) {
         this.width = width;
         this.height = height;
         this.placeables = placeables;
+        shots = new ArrayList<>();
     }
 
     public boolean addPlaceable(Placeable placeable, Location location) {
@@ -39,6 +45,7 @@ public class Board implements Serializable {
     public void applyAction(Action action) {
         Weapon weapon = action.getWeapon();
         Location location = action.getLocation();
+        shots.add(location);
         weapon.applyToBoard(this, location);
     }
 
@@ -56,6 +63,10 @@ public class Board implements Serializable {
 
     public Map<Location, Placeable> getPlaceables() {
         return placeables;
+    }
+
+    public List<Location> getShots() {
+        return shots;
     }
 
     public int getWidth() {
@@ -85,5 +96,4 @@ public class Board implements Serializable {
 
         return false;
     }
-
 }
