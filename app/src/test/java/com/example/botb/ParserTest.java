4ebd4931778aca34ae90777fcbbfd05c4ab2316b
Christian Nyvoll <christiannyvoll@gmail.com>
2019-05-03 12:39:28 +0200

    Gradle version compatability with PowerMockito for tests, and test fix

--- app/src/test/java/com/example/botb/ParserTest.java
+++ app/src/test/java/com/example/botb/ParserTest.java
@@ -16,6 +16,7 @@ import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({Base64.class})
 public class ParserTest {
